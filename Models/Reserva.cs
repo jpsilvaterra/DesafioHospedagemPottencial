2221f9d1f984e306ed5636bcf7365517fbc3eb22
João Pedro Silva Terra <101167595+jpsilvaterra@users.noreply.github.com>
2023-01-13 13:30:17 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Reserva(Suite suite)
+        {
+            this.Suite = suite;
+
+        }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Número de hospedes superior a capacidade da Suite.");
+
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = 0;
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 10 / 100;
+            }
+
+            return valor;
+        }
+    }
+}
